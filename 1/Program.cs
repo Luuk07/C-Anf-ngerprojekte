8792b39e5d27bc17f41607d5bfec5f251021eb39
Luuk <156096951+Luuk07@users.noreply.github.com>
2024-12-30 16:21:29 +0100

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace C_Anfangsprojekt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Geben Sie fünf Werte an");
+            string wertOne = Console.ReadLine();
+            string wertTwo = Console.ReadLine();
+            string wertThree = Console.ReadLine();
+            string wertFour = Console.ReadLine();
+            string wertFive = Console.ReadLine();
+
+          
+            int valueOne = int.Parse(wertOne);
+            int valueTwo = int.Parse(wertTwo);
+            int valueThree = int.Parse(wertThree);
+            int valueFour = int.Parse(wertFour);
+            int valueFive = int.Parse(wertFive);
+
+            int[] values = {valueOne, valueTwo, valueThree, valueFour, valueFive };
+            int value = 0;
+            foreach (int n in values)
+            {
+                value += n;
+                
+            }
+            Console.WriteLine(value);
+
+
+
+
+
+
+        }
+    }
+}

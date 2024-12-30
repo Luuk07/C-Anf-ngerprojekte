d2c9169cbee02b53bc81718495873d65bb5e053f
Luuk <156096951+Luuk07@users.noreply.github.com>
2024-12-30 16:22:58 +0100

    Add files via upload

---
+++ Bankaccount/Bankaccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankaccount
+{
+    internal class Bankaccount
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public int Ammount { get; set;}
+
+        public Bankaccount(string username, string password, int ammount)
+        {
+            Username = username;
+            Password = password;
+            Ammount = ammount;
+        }
+
+        public int Einzahlen()
+        {
+            Console.WriteLine("Gib den Betrag den du Einzahlen willst ein");
+            int inputAmmount = int.Parse(Console.ReadLine());
+            int newAmmount = inputAmmount + Ammount;
+            Ammount = newAmmount;
+            return Ammount;
+        }
+
+        public int Auszahlen()
+        {
+            Console.WriteLine("Gib den Betrag den du Auszahlen willst ein");
+            int inputAmmount = int.Parse(Console.ReadLine());
+            int newAmmount = Ammount - inputAmmount;
+            Ammount = newAmmount;
+            if (Ammount < 0)
+            {
+                Console.WriteLine("Achtung dein Guthaben ist negativ.");
+            }
+            return Ammount;
+        }
+
+        public void CreatKonto()
+        {
+            Console.WriteLine("Gib deine ID ein");
+            Username = Console.ReadLine();
+            Console.WriteLine("Gib dein Passwort ein");
+            Password = Console.ReadLine();
+            Console.WriteLine("Gib dein Guthaben ein");
+            Ammount = int.Parse(Console.ReadLine());
+        }
+
+        public void Login()
+        {
+            Console.WriteLine("Gib deine ID ein");
+            string inputUsername = Console.ReadLine();
+            Console.WriteLine("Gib dein Passwort ein");
+            string inputPassword = Console.ReadLine();
+
+            if (inputUsername == Username && inputPassword == Password)
+            {
+                Console.WriteLine("Korrekt");
+            }
+            else
+            {
+                Console.WriteLine("Falsch");
+                Login();
+            }
+
+
+        }
+
+
+
+        
+    }
+}
---
+++ Bankaccount/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Bankaccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+           
+            
+            Bankaccount myBankaccount = new Bankaccount("", "", 0);
+            myBankaccount.CreatKonto();
+
+            while (run) 
+            {
+                Console.WriteLine("1=Einzahlen, 2= Auszahlen, 3= Anzeigen, 4 = Ausloggen, 5= Programm schließen");
+                int answer = int.Parse(Console.ReadLine());
+                switch (answer)
+                {
+                    case 1:
+                        myBankaccount.Einzahlen();
+                        break;
+                    case 2:
+                        myBankaccount.Auszahlen();
+                        break;
+                    case 3:
+                        Console.WriteLine(myBankaccount.Ammount);
+                        break;
+                    case 4:
+                        myBankaccount.Login();
+                        break;
+                    case 5:
+                        Console.WriteLine("Tschüß");
+                        run = false;
+                        break;
+
+                        
+                }
+
+            }
+            
+            
+        }
+    }
+}
